011f0418c5e5a71c5ce043545890beffebdeeb56
Andrew Milgrom <andrew.milgrom@gmail.com>
2016-11-10 21:04:41 -0500

    remove aliased routes, do not work with devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,5 @@ Rails.application.routes.draw do
   root 'application#hello'
 
   devise_for :users
-  # authentication aliases
-  get    '/signup',  to: 'devise/registrations#new'
-  post   '/signup',  to: 'devise/registrations#create'
-  get    '/login',   to: 'devise/sessions#new'
-  post   '/login',   to: 'devise/sessions#create'
-  delete '/logout',  to: 'devise/sessions#destroy'
 
 end
